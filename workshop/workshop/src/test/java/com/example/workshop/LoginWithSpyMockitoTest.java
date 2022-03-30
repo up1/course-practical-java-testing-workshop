803af1c23617c9f0fe0b833a9cd995dd3442cdab
up1 <somkiat.p@gmail.com>
2022-03-30 09:54:13 +0700

    @Mock vs @Spy

--- workshop/workshop/src/test/java/com/example/workshop/LoginWithSpyMockitoTest.java
+++ workshop/workshop/src/test/java/com/example/workshop/LoginWithSpyMockitoTest.java
@@ -4,11 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -17,11 +18,15 @@ class LoginWithSpyMpckitoTest {
     @InjectMocks
     private Login login;
 
-    @Mock
+    @Spy
     private UserRepository userRepository;
 
     @Test
     public void called_login_from_repository() {
+        // Initial
+//        when(userRepository.login("somkiat", "xxxx")).thenReturn(false);
+        doReturn(false)
+                .when(userRepository).login("somkiat", "xxxx");
         // Call target method
         login.process("somkiat", "xxxx");
         // Assertion
