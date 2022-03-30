63ab3ccc05b53aa06a837b7ea2aabaa4a84aa342
up1 <somkiat.p@gmail.com>
2022-03-30 15:41:38 +0700

    Mocito style

--- workshop/workshop/src/test/java/com/example/workshop/auth/AuthControllerTest.java
+++ workshop/workshop/src/test/java/com/example/workshop/auth/AuthControllerTest.java
@@ -7,6 +7,8 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.when;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
@@ -26,7 +28,11 @@ class AuthControllerTest {
     @Test
     void login_success() {
         // Arrange
-        when(ldapClient.check("somkiat")).thenReturn(true);
+        when(ldapClient.check("somkiat")).thenReturn(true); // Suggest from
+        // Mockito
+//        given(ldapClient.check("somkiat")).willReturn(true);
+//        doReturn(true).when(ldapClient).check("somkiat");
+
         when(userRepository.findUser("somkiat","xxxx")).thenReturn(true);
         when(redisClient.createSession("somkiat")).thenReturn("123456");
 
